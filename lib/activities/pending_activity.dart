06a83468abf470c6926b285fab5c04910fc9a3ed
3DUTEX <t3xdev@gmail.com>
2024-03-16 15:16:18 -0300

    criar componente que mostre 'sem tarefas no momento' quando não tiver tarefas pendentes

--- lib/activities/pending_activity.dart
+++ lib/activities/pending_activity.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lista_de_tarefas/helpers/show_toast.dart';
 import 'package:lista_de_tarefas/models/task.dart';
 import 'package:lista_de_tarefas/repositories/task_repository.dart';
-import 'package:path/path.dart';
 
 class PendingActivity extends StatefulWidget {
   const PendingActivity({super.key});
@@ -14,51 +13,8 @@ class PendingActivity extends StatefulWidget {
 class _PendingActivityState extends State<PendingActivity> {
   TaskRepository taskRepository = TaskRepository();
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: taskRepository.getTasks(status: 0),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<Task> data = snapshot.data!;
-          return Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Pendentes",
-                  style: TextStyle(fontSize: 20),
-                ),
-                Expanded(
-                    child: ListView.separated(
-                        separatorBuilder: (context, index) => const Divider(),
-                        itemCount: data.length,
-                        itemBuilder: (context, index) {
-                          return TaskItem(
-                              repository: taskRepository, task: data[index]);
-                        }))
-              ],
-            ),
-          );
-        }
-
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-  }
-}
-
-class TaskItem extends StatelessWidget {
-  final Task task;
-  final TaskRepository repository;
-
-  const TaskItem({required this.repository, required this.task, super.key});
-
-  Future<void> handleClickDelete(BuildContext context) async {
-    bool isDeleted = await repository.delete(task.id);
+  Future<void> handleClickDelete(BuildContext context, int id) async {
+    bool isDeleted = await taskRepository.delete(id);
 
     if (!isDeleted) {
       if (context.mounted) {
@@ -76,25 +32,88 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(task.title),
-      subtitle: Wrap(
+    return FutureBuilder(
+      future: taskRepository.getTasks(status: 0),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // se não tiver dados
+          if (snapshot.data!.isEmpty) {
+            return const SemTarefas();
+          }
+
+          // se tiver dados
+          return Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Pendentes",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Expanded(
+                    child: ListView.separated(
+                        separatorBuilder: (context, index) => const Divider(),
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          Task task = snapshot.data![index];
+                          return ListTile(
+                            title: Text(task.title),
+                            subtitle: Wrap(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 5, bottom: 5),
+                                  child: Text("Descrição: ${task.desc}"),
+                                ),
+                                Text("Data conclusão : ${task.date}")
+                              ],
+                            ),
+                            trailing: Wrap(
+                              spacing: 20,
+                              children: [
+                                const Icon(Icons.edit),
+                                GestureDetector(
+                                  onTap: () async {
+                                    await handleClickDelete(context, task.id);
+                                    setState(() {});
+                                  },
+                                  child: const Icon(Icons.delete),
+                                )
+                              ],
+                            ),
+                          );
+                        }))
+              ],
+            ),
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
+
+class SemTarefas extends StatelessWidget {
+  const SemTarefas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 5, bottom: 5),
-            child: Text("Descrição: ${task.desc}"),
+          Text(
+            "Pendentes",
+            style: TextStyle(fontSize: 20),
           ),
-          Text("Data conclusão : ${task.date}")
-        ],
-      ),
-      trailing: Wrap(
-        spacing: 20,
-        children: [
-          const Icon(Icons.edit),
-          GestureDetector(
-            onTap: () => handleClickDelete(context),
-            child: const Icon(Icons.delete),
-          )
+          Text("Sem tarefas no momento!")
         ],
       ),
     );
