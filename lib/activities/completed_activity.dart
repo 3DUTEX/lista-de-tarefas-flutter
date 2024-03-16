f4bf7aa09948706fa210e6c9c7d9f74a630e277c
3DUTEX <t3xdev@gmail.com>
2024-03-16 17:24:03 -0300

    REFACTOR: refatorar design da task na tela concluídas

--- lib/activities/completed_activity.dart
+++ lib/activities/completed_activity.dart
@@ -43,48 +43,48 @@ class _CompletedActivityState extends State<CompletedActivity> {
 
           // se tiver dados
           return Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Concluídas",
-                  style: TextStyle(fontSize: 20),
-                ),
-                Expanded(
-                    child: ListView.separated(
-                        separatorBuilder: (context, index) => const Divider(),
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          Task task = snapshot.data![index];
-                          return ListTile(
-                            title: Text(task.title),
-                            subtitle: Wrap(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.only(top: 5, bottom: 5),
-                                  child: Text("Descrição: ${task.desc}"),
-                                ),
-                                Text("Data conclusão : ${task.date}")
-                              ],
+            padding: const EdgeInsets.all(5),
+            child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  Task task = snapshot.data![index];
+                  return Card(
+                    color: Colors.indigo,
+                    elevation: 16,
+                    child: ListTile(
+                      title: Text(task.title,
+                          style: const TextStyle(color: Colors.white)),
+                      subtitle: Wrap(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5, bottom: 5),
+                            child: Text(
+                              "Descrição: ${task.desc}",
+                              style: const TextStyle(color: Colors.white),
                             ),
-                            trailing: Wrap(
-                              spacing: 20,
-                              children: [
-                                GestureDetector(
-                                  onTap: () async {
-                                    await handleClickDelete(context, task.id);
-                                    setState(() {});
-                                  },
-                                  child: const Icon(Icons.delete),
-                                )
-                              ],
+                          ),
+                          Text("Data conclusão : ${task.date}",
+                              style: const TextStyle(color: Colors.white))
+                        ],
+                      ),
+                      trailing: Wrap(
+                        spacing: 20,
+                        children: [
+                          GestureDetector(
+                            onTap: () async {
+                              await handleClickDelete(context, task.id);
+                              setState(() {});
+                            },
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.white,
                             ),
-                          );
-                        }))
-              ],
-            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
           );
         }
 
